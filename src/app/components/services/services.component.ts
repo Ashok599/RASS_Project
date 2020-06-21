95dcab33c58c5d01b88743fd1c616e3d12691980
Ashok55 <ashok.gaikwad@straviso.com>
2020-06-21 14:23:41 +0530

    added updated servieces

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -24,54 +24,86 @@ export class ServicesComponent implements OnInit {
 
   ngOnInit() {
     this.constrObj = {
-      "Architectural/ Structural": {
+      "Architectural/ Structural Design": {
         "isEnabled": true,
         "imgSrc": "../../../../../assets/images/Architectural-Structural.jpg",
         "values": [
-          "Master Planning",
-          "Design Basis Report",
-          "Concept Designs",
-          "3D Views",
-          "Working Drawings",
-          "LEED certification",
-          "Integrated Drawings",
-          "As-Built Drawings",
-          "BOQ/ Tendering"
+          "Formulation of the Concept",
+          "Preparing Design basis report",
+          "LEED/IGBC Certification",
+          "Developing Master Plan and Layouts",
+          "Detailed Design",
+          "3 Dimensional Study and views",
+          "Working Drawings - Plans, Sections, Elevations and Details",
+          "Structural Analysis and Design",
+          "Preparing detail structural drawings",
+          "Finalizing Specifications, Bill of Quantities",
+          "As Built Drawings"
         ]
       },
-      "Electrical Engineering": {
+      "Electrical Design": {
         "isEnabled": false,
         "imgSrc": "../../../../../assets/images/Electrical-Engineering.jpg",
         "values": [
-          "HT/LT",
-          "DG/ Transformer/ Panels",
-          "Lighting",
-          "3D Views",
-          "IT/ Telecommunication",
-          "Fire Alarm",
-          "Data/ Voice",
-          "Solar System",
-          "BOQ/ Tendering"
+          "Preparation of design basis report",
+          "Liasoning with Power Supply Boards",
+          "HT and LT Distribution Design and layout",
+          "High tension & low tension substation layout",
+          "System Power Flow, Study Fault Level Analysis",
+          "Design plant lightning, street lighting, yard lightning etc",
+          "Emergency power supply & distribution ",
+          "Relay Coordination and Settings",
+          "Earthing and Lightning Protection",
+          "Control Automation and Instrumentation plan",
+          "Transformer and Generator Protection",
+          "Prepare scheme for Data/Voice Cabling",
+          "Design of solar system",
+          "Testing and Commissioning Assistance",
+          "Preparation of tender document, BOQ and estimate"
         ]
       },
-      "Mechanical Engineering": {
+      "HVAC and Ventilation Design": {
         "isEnabled": false,
         "imgSrc": "../../../../../assets/images/Mechanical-Engineering.jpg",
         "values": [
-          "Fire Fighting",
-          "Process Piping",
-          "Utility Piping",
-          "HVAC",
-          "ETP/ STP",
-          "LPG/ CNG/ Steam",
-          "BOQ/ Tendering"
+          "Evaluation of Ventilation and Atmospheric Control Requirements  ",
+          "Heat Load Calculations",
+          "Design entire HVAC System",
+          "Clean Room Assessment and Design",
+          "Selection of Proper Equipment",
+          "Preparation of detailed working Drawings",
+          "Detailed Drawing and Design for Ducting, Equipment, Piping etc",
+          "Preparation of tender document, BOQ and estimate",
+          "Inspection, Validation, Testing and Commissioning"
+        ]
+      },
+      "Fire Fighting Design": {
+        "isEnabled": false,
+        "imgSrc": "../../../../../assets/images/Mechanical-Engineering.jpg",
+        "values": [
+          "Design of firefighting system based on statutory norms and requirements",
+          "Preparation of detailed working Drawings",
+          "Preparation of tender document, BOQ and estimate",
+          "Installation and Testing Supervision"
+        ]
+      },
+      "Interior and Landscape Design": {
+        "isEnabled": false,
+        "imgSrc": "../../../../../assets/images/Mechanical-Engineering.jpg",
+        "values": [
+          "Formulation of the concept", 
+          "Design Grass, trees & shrubs pattern as per climatic conditions & availability of substrat",
+          "Selection of various Trees, Shrubs, Border Flowering Plants, Lawns etc. for Landscaping",
+          "Design good irrigation system for Landscaping work using recycled water",
+          "Preparation of detailed working Drawings in accordance with concept",
+          "Preparation of tender document, BOQ and estimate"
         ]
       },
       "Pre-Construction Stage": {
         "isEnabled": false,
         "imgSrc": "../../../../../assets/images/preConstruction.jpg",
         "values": [
-          "Liasoning",
+          "Statutory Liasoning",
           "Constructability Review",
           "Value Engineering",
           "Tracking Design Deliverables",
@@ -87,8 +119,9 @@ export class ServicesComponent implements OnInit {
           "Final Reconciliation",
           "Extra Item Settlement",
           "Project Completion Report",
-          "Assistance in As-Built",
-          "Drawings",
+          "Assistance in As-Built Drawings",
+          "Closing of Snag list Items",
+          "Commercial close-out",
           "Handover Documentation"
         ]
       },
@@ -97,20 +130,22 @@ export class ServicesComponent implements OnInit {
         "imgSrc": "../../../../../assets/images/subServices/time-management2.png",
         "values": [
           "Milestone Schedule",
+          "Baseline Schedule",
           "Critical Path Review",
-          "Schedule Review",
-          "Resources Requirements",
+          "Tracking of Schedule",
+          "Recovery schedule in case of delay",
           "Anticipated Cash Flow",
-          "Progress/Performance review"
+          "Progress/Performance review",
+          "Time Impact Analysis"
         ]
       },
       "Cost Management": {
         "isEnabled": false,
         "imgSrc": "../../../../../assets/images/subServices/cost-management1.png",
         "values": [
-          "Actual v/s Budgeted costs",
-          "Bill Checking",
-          "Monitor cash flow",
+          "Actual v/s Budgeted costs ",
+          "Scrutiny of contractors bill and certification",
+          "Prepare and Monitor cash flow",
           "Evaluate change/variations impact",
           "Develop cost forecasts"
         ]
@@ -135,7 +170,8 @@ export class ServicesComponent implements OnInit {
           "Safety Audits",
           "Work Permit Implementation",
           "Daily Supervision",
-          "Safety Training"
+          "Safety Training",
+          "Safety Documentation",
         ]
       },
        "Risk Management": {
